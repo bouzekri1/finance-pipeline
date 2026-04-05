f5ee834b4229d10fa10a6a9ab0023c27c9df6388
Abdessamad Bouzekri <bouzekri144@gmail.com>
2026-04-05 22:14:06 +0200

    feat: add Airflow DAG orchestrating full pipeline (fetch → load → dbt)

---
+++ airflow/dags/finance_pipeline_dag.py
@@ -0,0 +1,38 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.operators.bash import BashOperator
+from datetime import datetime
+import sys
+import os
+sys.path.insert(0, '/opt/airflow/ingestion')
+
+import fetch_sirene
+import load_many_to_redshift
+
+with DAG(
+    dag_id="finance_pipeline",
+    start_date=datetime(2026, 1, 1),
+    schedule_interval="@daily",
+    catchup=False
+) as dag:
+
+    # Tâche 1
+    t1 = PythonOperator(
+        task_id="fetch_sirene",
+        python_callable=fetch_sirene.main
+    )
+
+    # Tâche 2
+    t2 = PythonOperator(
+        task_id="load_to_redshift",
+        python_callable=load_many_to_redshift.main
+    )
+
+    # Tâche 3
+    t3 = BashOperator(
+        task_id="dbt_run",
+        bash_command="cd /opt/airflow/dbt && dbt run && dbt run"
+    )
+
+    # Ordre d'exécution
+    t1 >> t2 >> t3
